b59acae778f8373e222e83c59b42cc85548b6ec9
kakeru <okinawan.kkr0713@icloud.com>
2019-11-01 13:50:43 +0900

    mailer生成

---
+++ mailer_text.rb
@@ -0,0 +1,14 @@
+
+
+instCloneで間違ったから
+
+とりあえず１、３はやってしまったから
+２をやってくれ
+
+
+
+1 $ rails g scaffold Contact name:string email:string content:text
+
+2 $ rails db:migrate
+
+3  $ rails g mailer ContactMailer
