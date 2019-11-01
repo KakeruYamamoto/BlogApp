eebde6d614ab11294e83694c94068c0af4e42f50
kakeru <okinawan.kkr0713@icloud.com>
2019-11-01 14:01:26 +0900

    お問い合わせ先記述

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,7 @@
 class ContactMailer < ApplicationMailer
+  def contact_mail(contact)
+    @contact = contact#この記述でお問い合わせをした人の情報をViewファイルに渡すことができる。
+
+    mail to: "okinawan.kkr0713@icloud.com", subject: "お問い合わせの確認メール"
+  end#mail toは自分の。
 end
