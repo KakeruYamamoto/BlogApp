ee6056993e1043f4a0107242e2acedd3d40f0fa9
kakeru <okinawan.kkr0713@icloud.com>
2019-11-10 15:29:16 +0900

    routesにresources:comments記述

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :blogs do
+    resources :comments
     collection do #idを必要としない固有のルーティングを生成。
       #member doを使うとidを必要とする固有のルーティングを生成。
       #resources :blog とすると、どのパスにもideaを必要としないルーティングを生成できる。
