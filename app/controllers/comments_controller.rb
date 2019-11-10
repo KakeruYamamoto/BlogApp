76dabea68ec2a641e7446f408ff3dd792ef0cff4
kakeru <okinawan.kkr0713@icloud.com>
2019-11-10 15:52:49 +0900

    controller/commen/create修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,22 @@
 class CommentsController < ApplicationController
-  
+  # コメントを保存、投稿するためのアクションです。
   def create
-    @blog = Blog.new(blog_params)
-
+    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)#@blogのidをblog_idにあらかじめ含んだ状態のCommentインスタンスをnew（作成）する
+    # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
-      if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.js { render :index }
+      if @comment.save
+        format.html { redirect_to blog_path(@blog) }
       else
-        format.html { render :new }
-        format.js { render :index }
+        format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした...' }
       end
     end
   end
 
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
+  end
 end
