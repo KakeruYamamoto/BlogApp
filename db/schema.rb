dd3a9775b580ad7c4f97bb86cfdf579691af1da3
kakeru <okinawan.kkr0713@icloud.com>
2019-11-10 15:31:23 +0900

    スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_043952) do
+ActiveRecord::Schema.define(version: 2019_11_10_062927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2019_11_01_043952) do
     t.bigint "user_id"
     t.string "timestamps"
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -58,5 +66,6 @@ ActiveRecord::Schema.define(version: 2019_11_01_043952) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
   add_foreign_key "feeds", "users"
 end
