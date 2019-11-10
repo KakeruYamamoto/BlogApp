289f915a3e83f44cd994d95d16be0f24c4dc0912
kakeru <okinawan.kkr0713@icloud.com>
2019-11-10 15:43:19 +0900

    controller/commen/create記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
+  
   def create
+    @blog = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.js { render :index }
+      else
+        format.html { render :new }
+        format.js { render :index }
+      end
+    end
   end
+
 end
