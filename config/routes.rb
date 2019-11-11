1112d92c125b9e883da106912a4fd6326f26172a
kakeru <okinawan.kkr0713@icloud.com>
2019-11-11 18:11:44 +0900

    routesにuser_resourcesにindex追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index]
   resources :blogs do
     resources :comments
     collection do #idを必要としない固有のルーティングを生成。
