deb0048b4ac548dd82f0cbf85d372dc1ce22e266
kakeru <okinawan.kkr0713@icloud.com>
2019-11-11 15:43:42 +0900

    relationshipモデルにアソシエーション記述

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+  belongs_to :followed, class_name: "User"
+  belongs_to :follower, class_name: "User"
+  #こちらもuserモデルに記述したアソシエーション同様関連づけるuserモデルは同じだけれども
+  #フォローするものされるものを区別するために名前を慣習的でない名にしている
+  
 end
