4bc6185e6d894d2d19ca3f6f3605c71bdb1ad74e
kakeru <okinawan.kkr0713@icloud.com>
2019-11-11 15:02:01 +0900

    relationsマイグレート追記

--- db/migrate/20191111055019_create_relationships.rb
+++ db/migrate/20191111055019_create_relationships.rb
@@ -6,5 +6,9 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+    #add_index ~~ unique: trueと追記することで[:follower_id, :followed_id]が同じ組み合わせで保存されないようにできる
   end
 end
